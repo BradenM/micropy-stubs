ac031121dd89ef9c843d88bb28e16dd23e9d5f75
Braden Mars <BradenM@users.noreply.github.com>
2019-06-19 15:05:48 -0500

    feat: Generated Modules/Info For MPOfficial_V1.10_ESP32

--- micropython-official/v1.10/esp32/modules/apa106.pyi
+++ micropython-official/v1.10/esp32/modules/apa106.pyi
@@ -1,4 +1,5 @@
-# make_stub_files: Wed 19 Jun 2019 at 15:04:41
+# make_stub_files: Wed 19 Jun 2019 at 15:01:21
 
 from typing import Any, Dict, Optional, Sequence, Tuple, Union
 Node = Any
+class APA106(NeoPixel): ...
