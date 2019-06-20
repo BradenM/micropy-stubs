3ddffddf80c9c3df551250c0d1b45bb62fe416db
Braden Mars <BradenM@users.noreply.github.com>
2019-06-20 00:14:03 -0500

    refactor(files): Moved Packages to packages directory

---
+++ modules/websocket_helper.pyi
@@ -0,0 +1,6 @@
+# make_stub_files: Wed 19 Jun 2019 at 15:01:21
+
+from typing import Any, Dict, Optional, Sequence, Tuple, Union
+Node = Any
+def server_handshake(sock: Any) -> None: ...
+def client_handshake(sock: Any) -> None: ...
