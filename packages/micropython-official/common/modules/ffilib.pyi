e188f9a3ecfe8b429c908f6c1415b107d2974f46
Braden Mars <BradenM@users.noreply.github.com>
2019-06-20 02:10:03 -0500

    feat(stubs): Updated Package Stubs

---
+++ packages/micropython-official/common/modules/ffilib.pyi
@@ -0,0 +1,11 @@
+# make_stub_files: Thu 20 Jun 2019 at 02:09:42
+
+from typing import Any, Dict, Optional, Sequence, Tuple, Union
+Node = Any
+def open(name: str, maxver: Any=10, extra: Any=()) -> Any:
+    #   0: return l
+    # ? 0: return l
+    def libs() -> None: ...
+def libc() -> Any: ...
+    #   0: return open('libc',6)
+    # ? 0: return open(str, number)
