82203b18e489c7abfa20b4bd25e05a899553dee6
Braden Mars <BradenM@users.noreply.github.com>
2019-06-20 02:10:03 -0500

    feat(stubs): Updated Package Stubs

---
+++ common/modules/errno.pyi
@@ -0,0 +1,4 @@
+# make_stub_files: Thu 20 Jun 2019 at 02:09:42
+
+from typing import Any, Dict, Optional, Sequence, Tuple, Union
+Node = Any
---
+++ common/modules/urllib/urequest.pyi
@@ -0,0 +1,5 @@
+# make_stub_files: Thu 20 Jun 2019 at 02:09:42
+
+from typing import Any, Dict, Optional, Sequence, Tuple, Union
+Node = Any
+def urlopen(url: Any, data: Any=None, method: Any='GET') -> str: ...
