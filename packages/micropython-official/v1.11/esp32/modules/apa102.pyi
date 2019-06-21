56d4d96efc58305afa02ec391dfb09d31e813a19
Braden Mars <BradenM@users.noreply.github.com>
2019-06-20 23:07:37 -0500

    fix: Incorrect Stubs and Info for ESP32-micropython@1.11.0

--- packages/micropython-official/v1.11/esp32/modules/apa102.pyi
+++
@@ -1,7 +0,0 @@
-# make_stub_files: Wed 19 Jun 2019 at 13:56:39
-
-from typing import Any, Dict, Optional, Sequence, Tuple, Union
-Node = Any
-class APA102(NeoPixel):
-    def __init__(self, clock_pin: Any, data_pin: Any, n: int, bpp: Any=4) -> None: ...
-    def write(self) -> None: ...
