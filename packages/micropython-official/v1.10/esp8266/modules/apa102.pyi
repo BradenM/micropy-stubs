0ca8b37de67daaae6d96c796c2863d882abe1b97
Braden Mars <BradenM@users.noreply.github.com>
2019-06-21 00:46:01 -0500

    chore: Generated New Stub Packages

---
+++ packages/micropython-official/v1.10/esp8266/modules/apa102.pyi
@@ -0,0 +1,7 @@
+# make_stub_files: Fri 21 Jun 2019 at 00:44:04
+
+from typing import Any, Dict, Optional, Sequence, Tuple, Union
+Node = Any
+class APA102(NeoPixel):
+    def __init__(self, clock_pin: Any, data_pin: Any, n: int, bpp: Any=4) -> None: ...
+    def write(self) -> None: ...
---
+++ packages/micropython-official/v1.9.4/esp8266/modules/port_diag.pyi
@@ -0,0 +1,5 @@
+# make_stub_files: Fri 21 Jun 2019 at 00:44:00
+
+from typing import Any, Dict, Optional, Sequence, Tuple, Union
+Node = Any
+def main() -> None: ...
