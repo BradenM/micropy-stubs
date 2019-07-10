8230b2141e53bdbf56705c870366abdd6646fecc
Braden Mars <BradenM@users.noreply.github.com>
2019-07-10 04:13:43 -0500

    feat: Updated Micropython Stub Packages
    Updated packages in Micropython Firmware

---
+++ stubs/uasyncio/core.py
@@ -0,0 +1,75 @@
+"""
+Module: 'uasyncio.core' on esp8266 v1.9.3
+"""
+# MCU: (sysname='esp8266', nodename='esp8266', release='2.0.0(5a875ba)', version='v1.9.3-8-g63826ac5c on 2017-11-01', machine='ESP module with ESP8266')
+# Stubber: 1.1.2
+DEBUG = 0
+
+class EventLoop:
+    ''
+    def call_at_():
+        pass
+
+    def call_later():
+        pass
+
+    def call_later_ms():
+        pass
+
+    def call_soon():
+        pass
+
+    def close():
+        pass
+
+    def create_task():
+        pass
+
+    def run_forever():
+        pass
+
+    def run_until_complete():
+        pass
+
+    def stop():
+        pass
+
+    def time():
+        pass
+
+    def wait():
+        pass
+
+
+class IORead:
+    ''
+
+class IOReadDone:
+    ''
+
+class IOWrite:
+    ''
+
+class IOWriteDone:
+    ''
+
+class PollEventLoop:
+    ''
+    def add_reader():
+        pass
+
+    def add_writer():
+        pass
+
+    def remove_reader():
+        pass
+
+    def remove_writer():
+        pass
+
+    def wait():
+        pass
+
+
+class SleepMs:
+    ''
