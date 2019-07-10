b5d9dfb24cd374e53ac1cda3f3223525422f9bf2
Braden Mars <BradenM@users.noreply.github.com>
2019-07-10 04:14:54 -0500

    feat: esp32_LoBo Stub Package

---
+++ stubs/_thread.py
@@ -0,0 +1,81 @@
+"""
+Module: '_thread' on esp32_LoBo 3.2.24
+"""
+# MCU: (sysname='esp32_LoBo', nodename='esp32_LoBo', release='3.2.24', version='ESP32_LoBo_v3.2.24 on 2018-09-06', machine='ESP32 board with ESP32')
+# Stubber: 1.2.0
+EXIT = 67108864
+PAUSE = 16777216
+RESUME = 33554432
+RUNNING = 0
+STATUS = 134217728
+STOP = 67108864
+SUSPEND = 16777216
+SUSPENDED = 1
+TERMINATED = -1
+WAITING = 2
+def allowsuspend():
+    pass
+
+def getMainID():
+    pass
+
+def getReplID():
+    pass
+
+def getSelfName():
+    pass
+
+def getThreadName():
+    pass
+
+def getmsg():
+    pass
+
+def getnotification():
+    pass
+
+def isnotified():
+    pass
+
+def list():
+    pass
+
+def lock():
+    pass
+
+def mainAcceptMsg():
+    pass
+
+def notify():
+    pass
+
+def replAcceptMsg():
+    pass
+
+def resume():
+    pass
+
+def sendmsg():
+    pass
+
+def stack_size():
+    pass
+
+def start_new_thread():
+    pass
+
+def status():
+    pass
+
+def stop():
+    pass
+
+def suspend():
+    pass
+
+def unlock():
+    pass
+
+def wait():
+    pass
+
