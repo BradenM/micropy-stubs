060a8fd7746cc6a6c257f43378a3b8dabb03b143
Braden Mars <BradenM@users.noreply.github.com>
2019-07-17 02:49:13 -0500

    feat: Update All Packages

--- packages/micropython-official/frozen/utokenize.py
+++ packages/micropython-official/frozen/utokenize.py
@@ -3,10 +3,8 @@ from token import *
 from ucollections import namedtuple
 
 
-COMMENT = N_TOKENS + 0
-NL = N_TOKENS + 1
-ENCODING = N_TOKENS + 2
-tok_name[COMMENT] = "COMMENT"
+NL = 55
+ENCODING = 56
 tok_name[NL] = "NL"
 tok_name[ENCODING] = "ENCODING"
 
@@ -14,8 +12,8 @@ tok_name[ENCODING] = "ENCODING"
 class TokenInfo(namedtuple("TokenInfo", ("type", "string", "start", "end", "line"))):
 
     def __str__(self):
-        return "TokenInfo(type=%d (%s), string=%r, startl=%d, line=%r)" % (
-            self.type, tok_name[self.type], self.string, self.start, self.line
+        return "TokenInfo(type=%d (%s), string=%r, line=%r)" % (
+            self.type, tok_name[self.type], self.string, self.line
         )
 
 
@@ -28,34 +26,24 @@ def get_indent(l):
 def tokenize(readline):
 
     indent = 0
-    indent_lvl = 0
-    lineno = 0
 
     yield TokenInfo(ENCODING, "utf-8", 0, 0, "")
 
     while True:
         l = readline()
-        lineno += 1
         org_l = l
         if not l:
             break
         i, l = get_indent(l)
 
         if l == "\n":
-            yield TokenInfo(NL, l, lineno, 0, org_l)
-            continue
-
-        if l.startswith("#"):
-            yield TokenInfo(COMMENT, l.rstrip("\n"), lineno, 0, org_l)
-            yield TokenInfo(NL, "\n", lineno, 0, org_l)
+            yield TokenInfo(NL, l, 0, 0, org_l)
             continue
 
         if i > indent:
-            yield TokenInfo(INDENT, " " * i, lineno, 0, org_l)
-            indent_lvl += 1
+            yield TokenInfo(INDENT, " " * (i - indent), 0, 0, org_l)
         elif i < indent:
-            yield TokenInfo(DEDENT, "", lineno, 0, org_l)
-            indent_lvl -= 1
+            yield TokenInfo(DEDENT, "", 0, 0, org_l)
         indent = i
 
         while l:
@@ -64,24 +52,20 @@ def tokenize(readline):
                 while l and (l[0].isdigit() or l[0] == "."):
                     t += l[0]
                     l = l[1:]
-                yield TokenInfo(NUMBER, t, lineno, 0, org_l)
-            elif l[0].isalpha() or l.startswith("_"):
+                yield TokenInfo(NUMBER, t, 0, 0, org_l)
+            elif l[0].isalpha():
                 name = ""
-                while l and (l[0].isalpha() or l[0].isdigit() or l.startswith("_")):
+                while l and (l[0].isalpha() or l[0].isdigit()):
                     name += l[0]
                     l = l[1:]
-                yield TokenInfo(NAME, name, lineno, 0, org_l)
+                yield TokenInfo(NAME, name, 0, 0, org_l)
             elif l[0] == "\n":
-                yield TokenInfo(NEWLINE, "\n", lineno, 0, org_l)
+                yield TokenInfo(NEWLINE, "\n", 0, 0, org_l)
                 break
             elif l[0].isspace():
                 l = l[1:]
             else:
-                yield TokenInfo(OP, l[0], lineno, 0, org_l)
+                yield TokenInfo(OP, l[0], 0, 0, org_l)
                 l = l[1:]
 
-    while indent_lvl:
-        yield TokenInfo(DEDENT, "", lineno, 0, "")
-        indent_lvl -= 1
-
-    yield TokenInfo(ENDMARKER, "", lineno, 0, "")
+    yield TokenInfo(ENDMARKER, "", 0, 0, "")
