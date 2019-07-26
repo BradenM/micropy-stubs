de3a46cc7522aca84d6ec41a6339a30f7de45e61
Braden Mars <BradenM@users.noreply.github.com>
2019-07-25 22:19:05 -0500

    feat: Add umqtt.simple, uctypeslib modules to micropython

---
+++ frozen/uctypeslib2.pyi
@@ -0,0 +1,10 @@
+# make_stub_files: Thu 25 Jul 2019 at 22:01:29
+
+from typing import Any, Dict, Optional, Sequence, Tuple, Union
+Node = Any
+def pprint(desc: Any, stream: Any=None, cur_ind: Any=0) -> Any:
+    #   0: return v&2147483647>>31-VAL_TYPE_BITS
+    # ? 0: return v&number>>number-VAL_TYPE_BITS
+    def valtype(v: Any) -> Any: ...
+        #   0: return v&2147483647>>31-VAL_TYPE_BITS
+        # ? 0: return v&number>>number-VAL_TYPE_BITS
