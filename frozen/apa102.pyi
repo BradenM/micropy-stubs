e8789944efe6bc25b06fb1fa9613d434608c1c70
Braden Mars <BradenM@users.noreply.github.com>
2019-08-20 16:00:33 -0500

    feat(pycopy): ESP8266 v2.11.0.5 Stubs

---
+++ frozen/apa102.pyi
@@ -0,0 +1,7 @@
+# make_stub_files: Tue 20 Aug 2019 at 15:59:40
+
+from typing import Any, Dict, Optional, Sequence, Tuple, Union
+Node = Any
+class APA102(NeoPixel):
+    def __init__(self, clock_pin: Any, data_pin: Any, n: int, bpp: Any=4) -> None: ...
+    def write(self) -> None: ...
---
+++ frozen/port_diag.pyi
@@ -0,0 +1,5 @@
+# make_stub_files: Tue 20 Aug 2019 at 15:59:40
+
+from typing import Any, Dict, Optional, Sequence, Tuple, Union
+Node = Any
+def main() -> None: ...
