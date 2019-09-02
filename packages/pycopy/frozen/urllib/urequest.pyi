47586720ce65efaa1047ba60ac053b1f474c4632
Braden Mars <bradenmars@bradenmars.me>
2019-09-02 04:32:44 -0500

    chore(pycopy): Update all Pycopy Packages

--- packages/pycopy/frozen/urllib/urequest.pyi
+++ packages/pycopy/frozen/urllib/urequest.pyi
@@ -1,4 +1,4 @@
-# make_stub_files: Tue 20 Aug 2019 at 15:46:00
+# make_stub_files: Mon 02 Sep 2019 at 04:16:29
 
 from typing import Any, Dict, Optional, Sequence, Tuple, Union
 Node = Any
--- packages/pycopy/v2.11.0.5/esp8266/frozen/webrepl_setup.pyi
+++ packages/pycopy/v2.11.0.5/esp8266/frozen/webrepl_setup.pyi
@@ -1,4 +1,4 @@
-# make_stub_files: Tue 20 Aug 2019 at 15:59:40
+# make_stub_files: Mon 02 Sep 2019 at 04:16:32
 
 from typing import Any, Dict, Optional, Sequence, Tuple, Union
 Node = Any
