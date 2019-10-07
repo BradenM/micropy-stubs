cde8f613cc334ae2210e03d519cb4df1dec66ba2
Braden Mars <bradenmars@bradenmars.me>
2019-10-06 21:06:13 -0500

    chore: Update All Frozen Modules

--- frozen/functools.pyi
+++ frozen/functools.pyi
@@ -1,4 +1,3 @@
-# make_stub_files: Thu 25 Jul 2019 at 22:35:06
 
 from typing import Any, Dict, Optional, Sequence, Tuple, Union
 Node = Any
--- master/esp32_LoBo/frozen/microWebSocket.pyi
+++ master/esp32_LoBo/frozen/microWebSocket.pyi
@@ -1,4 +1,3 @@
-# make_stub_files: Wed 10 Jul 2019 at 03:21:45
 
 from typing import Any, Dict, Optional, Sequence, Tuple, Union
 Node = Any
