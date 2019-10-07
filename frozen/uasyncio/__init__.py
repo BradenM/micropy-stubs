978fbed4b929fcd2345947dfe61b09e95a7efce2
Braden Mars <bradenmars@bradenmars.me>
2019-10-06 20:55:09 -0500

    chore(pycopy): Update All Frozen

--- frozen/uasyncio/__init__.py
+++ frozen/uasyncio/__init__.py
@@ -12,8 +12,8 @@ def set_debug(val):
     global DEBUG, log
     DEBUG = val
     if val:
-        import logging
-        log = logging.getLogger("uasyncio")
+        import ulogging
+        log = ulogging.getLogger("uasyncio")
 
 
 class PollEventLoop(EventLoop):
@@ -242,23 +242,29 @@ def start_server(client_coro, host, port, backlog=10):
     ai = _socket.getaddrinfo(host, port, 0, _socket.SOCK_STREAM)
     ai = ai[0]
     s = _socket.socket(ai[0], ai[1], ai[2])
-    s.setblocking(False)
-
-    s.setsockopt(_socket.SOL_SOCKET, _socket.SO_REUSEADDR, 1)
-    s.bind(ai[-1])
-    s.listen(backlog)
-    while True:
-        if DEBUG and __debug__:
-            log.debug("start_server: Before accept")
-        yield IORead(s)
-        if DEBUG and __debug__:
-            log.debug("start_server: After iowait")
-        s2, client_addr = s.accept()
-        s2.setblocking(False)
-        if DEBUG and __debug__:
-            log.debug("start_server: After accept: %s", s2)
-        extra = {"peername": client_addr}
-        yield client_coro(StreamReader(s2), StreamWriter(s2, extra))
+    s2 = None
+    try:
+        s.setblocking(False)
+        s.setsockopt(_socket.SOL_SOCKET, _socket.SO_REUSEADDR, 1)
+        s.bind(ai[-1])
+        s.listen(backlog)
+        while True:
+            if DEBUG and __debug__:
+                log.debug("start_server: Before accept")
+            yield IORead(s)
+            if DEBUG and __debug__:
+                log.debug("start_server: After iowait")
+            s2, client_addr = s.accept()
+            s2.setblocking(False)
+            if DEBUG and __debug__:
+                log.debug("start_server: After accept: %s", s2)
+            extra = {"peername": client_addr}
+            yield client_coro(StreamReader(s2), StreamWriter(s2, extra))
+            s2 = None
+    finally:
+        if s2:
+            s2.close()
+        s.close()
 
 
 import uasyncio.core
