4f110cfb033acf8c5e11ea330c5021e96b0b85ce
GitHub Action <action@github.com>
2019-10-21 00:06:36 +0000

    chore: Update All Frozen

--- packages/pycopy/frozen/ffilib.pyi
+++ packages/pycopy/frozen/ffilib.pyi
@@ -8,3 +8,6 @@ def open(name: str, maxver: Any=10, extra: Any=()) -> Any:
 def libc() -> Any: ...
     #   0: return open('libc',6)
     # ? 0: return open(str, number)
+def makeref(typ: Any, val: Any) -> Any: ...
+    #   0: return uarray.array(typ,[val])
+    # ? 0: return uarray.array(typ, List[val])
