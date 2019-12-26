6648ee268fc108c7081589d1df4db8d7f6a1751b
Braden Mars <bradenmars@bradenmars.me>
2019-12-26 01:10:49 -0600

    feat(official): add ESP32 v1.12 Stub Package

---
+++ stubs/inisetup.py
@@ -0,0 +1,16 @@
+"""
+Module: 'inisetup' on esp32 1.12.0
+"""
+# MCU: (sysname='esp32', nodename='esp32', release='1.12.0', version='v1.12 on 2019-12-26', machine='ESP32 module with ESP32')
+# Stubber: 1.3.2
+bdev = None
+def check_bootsec():
+    pass
+
+def fs_corrupted():
+    pass
+
+def setup():
+    pass
+
+uos = None
