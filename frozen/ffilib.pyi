1d46e8acfef6ca2aaa273bfde497534bc2e8ed15
Braden Mars <bradenmars@bradenmars.me>
2020-01-31 16:53:41 -0600

    feat: update all frozen

--- frozen/ffilib.pyi
+++ frozen/ffilib.pyi
@@ -1,13 +1,13 @@
 
 from typing import Any, Dict, Optional, Sequence, Tuple, Union
 Node = Any
-def open(name: str, maxver: Any=10, extra: Any=()) -> Any:
+def open(name: str, maxver: Any=, extra: Any=()) -> Any:
     #   0: return l
     # ? 0: return l
     def libs() -> None: ...
 def libc() -> Any: ...
-    #   0: return open('libc',6)
-    # ? 0: return open(str, number)
-def makeref(typ: Any, val: Any=0) -> Any: ...
+    #   0: return open()
+    # ? 0: return open()
+def makeref(typ: Any, val: Any=) -> Any: ...
     #   0: return uarray.array(typ,[val])
     # ? 0: return uarray.array(typ, List[val])
--- frozen/os/path.pyi
+++ frozen/os/path.pyi
@@ -4,8 +4,8 @@ Node = Any
 def normcase(s: str) -> str: ...
 def normpath(s: str) -> str: ...
 def abspath(s: str) -> Union[Any, str]: ...
-    #   0: return os.getcwd()+'/'+s
-    # ? 0: return os.getcwd()+str+str
+    #   0: return os.getcwd()++s
+    # ? 0: return os.getcwd()++str
     #   1: return s
     #   1: return str
 def join(*args) -> Any: ...
@@ -13,36 +13,36 @@ def join(*args) -> Any: ...
     # ? 0: return res.encode()
     #   1: return res
     # ? 1: return res
-def split(path: Any) -> Union[Tuple[Any, str], Tuple[str, Any], Tuple[str, str]]: ...
-def splitdrive(path: Any) -> Tuple[str, Any]: ...
+def split(path: Any) -> Union[Tuple[, Any], Tuple[, ], Tuple[Any, str]]: ...
+def splitdrive(path: Any) -> Tuple[, Any]: ...
 def dirname(path: Any) -> Any: ...
-    #   0: return split(path)[0]
-    # ? 0: return split(path)[number]
+    #   0: return split(path)[]
+    # ? 0: return split(path)[]
 def basename(path: Any) -> Any: ...
-    #   0: return split(path)[1]
-    # ? 0: return split(path)[number]
+    #   0: return split(path)[]
+    # ? 0: return split(path)[]
 def exists(path: Any) -> Any: ...
     #   0: return os.access(path,os.F_OK)
     # ? 0: return os.access(path, os.F_OK)
-def isfile(path: Any) -> Union[Any, bool]: ...
+def isfile(path: Any) -> Optional[Any]: ...
     #   0: return stat.S_ISREG(mode)
     # ? 0: return stat.S_ISREG(mode)
-    #   1: return bool
-    #   1: return bool
-def isdir(path: Any) -> Union[Any, bool]: ...
+    #   1: return
+    #   1: return
+def isdir(path: Any) -> Optional[Any]: ...
     #   0: return stat.S_ISDIR(mode)
     # ? 0: return stat.S_ISDIR(mode)
-    #   1: return bool
-    #   1: return bool
-def islink(path: Any) -> Union[Any, bool]: ...
+    #   1: return
+    #   1: return
+def islink(path: Any) -> Optional[Any]: ...
     #   0: return stat.S_ISLNK(mode)
     # ? 0: return stat.S_ISLNK(mode)
-    #   1: return bool
-    #   1: return bool
+    #   1: return
+    #   1: return
 def expanduser(s: str) -> Union[Any, str]: ...
-    #   0: return h+s[1:]
+    #   0: return h+s[:]
     # ? 0: return h+str
-    #   1: return '/home/'+s[1:]
-    #   1: return str
+    #   1: return +s[:]
+    # ? 1: return +str
     #   2: return s
     #   2: return str
