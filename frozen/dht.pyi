8c044e6d269c11263c896bb2a11cf0de59bb8efe
Braden Mars <bradenmars@bradenmars.me>
2021-05-24 02:37:44 -0500

    feat(packages): update all frozen stubs

--- frozen/dht.pyi
+++ frozen/dht.pyi
@@ -2,19 +2,19 @@
 from typing import Any, Dict, Optional, Sequence, Tuple, Union
 Node = Any
 class DHTBase:
-    def __init__(self, pin: machine.Pin.pin) -> None: ...
+    def __init__(self, pin: machine.Pin) -> None: ...
     def measure(self) -> None: ...
 class DHT11(DHTBase):
     def humidity(self) -> Any: ...
-        #   0: return self.buf[0]
-        # ? 0: return self.buf[number]
+        #   0: return self.buf[]
+        # ? 0: return self.buf[]
     def temperature(self) -> Any: ...
-        #   0: return self.buf[2]
-        # ? 0: return self.buf[number]
+        #   0: return self.buf[]
+        # ? 0: return self.buf[]
 class DHT22(DHTBase):
     def humidity(self) -> Any: ...
-        #   0: return self.buf[0]<<8|self.buf[1]*0.1
-        # ? 0: return self.buf[number]<<number|self.buf[number]*number
+        #   0: return self.buf[]<<|self.buf[]*
+        # ? 0: return self.buf[]<<|self.buf[]*
     def temperature(self) -> Any: ...
         #   0: return t
         # ? 0: return t
