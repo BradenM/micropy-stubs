a4603f045ac5d00993c56d6755f52c79b5d704e9
Braden Mars <bradenmars@bradenmars.me>
2021-05-24 02:37:44 -0500

    feat(packages): update all frozen stubs

--- packages/micropython-official/frozen/ffilib.pyi
+++ packages/micropython-official/frozen/ffilib.pyi
@@ -1,10 +1,10 @@
 
 from typing import Any, Dict, Optional, Sequence, Tuple, Union
 Node = Any
-def open(name: str, maxver: Any=10, extra: Any=()) -> Any:
+def open(name: str, maxver: Any=, extra: Any=()) -> Any:
     #   0: return l
     # ? 0: return l
     def libs() -> None: ...
 def libc() -> Any: ...
-    #   0: return open('libc',6)
-    # ? 0: return open(str, number)
+    #   0: return open()
+    # ? 0: return open()
--- packages/pycom/v1.18.2/esp32/frozen/sqnscrc.pyi
+++ packages/pycom/v1.18.2/esp32/frozen/sqnscrc.pyi
@@ -2,5 +2,5 @@
 from typing import Any, Dict, Optional, Sequence, Tuple, Union
 Node = Any
 def fletcher32(data: Any) -> Any: ...
-    #   0: return s2<<16|s1
-    # ? 0: return str<<number|str
+    #   0: return s2<<|s1
+    # ? 0: return str<<|str
