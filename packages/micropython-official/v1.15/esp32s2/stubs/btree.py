05e6b78f44a2ab42ff23a3ebc1e6940f6b4d776e
Braden Mars <bradenmars@bradenmars.me>
2021-05-24 02:26:57 -0500

    feat(official/esp32s2): add stubs for official esp32s2 v1.15

---
+++ packages/micropython-official/v1.15/esp32s2/stubs/btree.py
@@ -0,0 +1,10 @@
+"""
+Module: 'btree' on micropython-esp32-1.15
+"""
+# MCU: {'ver': '1.15', 'port': 'esp32', 'arch': 'xtensawin', 'sysname': 'esp32', 'release': '1.15.0', 'name': 'micropython', 'mpy': 10757, 'version': '1.15.0', 'machine': 'ESP32-S2-SOALA-1 with ESP32S2', 'build': '', 'nodename': 'esp32', 'platform': 'esp32', 'family': 'micropython'}
+# Stubber: 1.3.9
+DESC = 2
+INCL = 1
+def open():
+    pass
+
